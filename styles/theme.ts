84b2932bb576c89533c05b100f76a20ce17497f5
Jeongs <kimm.jeongs@gmail.com>
2022-02-14 16:57:35 +0900

    :lipstick:  UI: base size, gap 추가 in theme

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,6 +1,6 @@
 const colors = {
   primary: '#000',
-  modalBackground:'rgba(0, 0, 0, 0.4)',
+  modalBackground: 'rgba(0, 0, 0, 0.4)',
   gray: '#808080',
   borderBottom: '#C4C4C4',
   disableGray: '#CCCCCC',
@@ -10,25 +10,17 @@ const colors = {
 };
 
 const size = {
-  buttonWidth: 76,
-  buttonHeight: 32,
-  borderRadius: 4,
-};
-
-const device = {
-  mobile: `(max-width: 768px)`,
-  desktop: `(max-width: 1440px)`,
+  baseHeight: 50,
 };
 
 const gap = {
-  base: 4,
+  base: 17,
 };
 
 const theme = {
   colors,
   size,
   gap,
-  device,
 };
 
 export type Theme = typeof theme;
