6281c75a971adbf337101023ec427bddcf313ca8
ludacirs <ludacirs@naver.com>
2022-02-14 23:43:05 +0900

    ♻️ Refactor: img 태그 Image 컴포넌트로 변경

--- components/base/ProductionImage/ProductionImage.tsx
+++ components/base/ProductionImage/ProductionImage.tsx
@@ -1,4 +1,4 @@
-import * as S from './Style';
+import Image from 'next/image';
 
 export interface ProductionImageProps {
   imageUrl: string;
@@ -6,10 +6,13 @@ export interface ProductionImageProps {
 }
 
 const ProductionImage = ({ imageUrl, size = 'small' }: ProductionImageProps) => {
+  const imageSize = size === 'small' ? 70 : 90;
   return (
-    <S.ProductionImageWrapper
-      size={size}
+    <Image
+      width={imageSize}
+      height={imageSize}
       src={'https://d2lfdrnysfmya6.cloudfront.net/032e1a0c-7b3c-4c18-bace-6b4ee09602fb.jpg'}
+      alt={'상품 이미지'}
     />
   );
 };
