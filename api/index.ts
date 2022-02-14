877644fa77292288bffee40dc3ec83727abef0c9
ludacirs <ludacirs@naver.com>
2022-02-15 03:21:21 +0900

    ✨ Feat: Home페이지 데이터 패칭 구현
    대분류는 staticProps로 받아오기,
    땡처리는 자주 바뀌기 때문에 useAxios를 이용해 페이지 진입마다 갱신

---
+++ api/index.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { Category, SoonItem } from 'types';
+import { Nested } from '../models/nested';
+
+export const getCategories = async () => {
+  try {
+    const { data } = await axios.get(`https://api2.ncnc.app/con-category1s`);
+    return data.conCategory1s as Category[];
+  } catch (e) {
+    console.log('카테고리(대분류)API 에러');
+  }
+};
+
+export const getSoonItems = async () => {
+  try {
+    const { data } = await axios.get(`https://api2.ncnc.app/con-items/soon`);
+    return data.conItems as SoonItem[];
+  } catch (e) {
+    console.log('땡처리 API 에러');
+  }
+};
+
+export const getNested = async (categoryId: number) => {
+  try {
+    const { data } = await axios.get(`https://api2.ncnc.app/con-category1s/${categoryId}/nested`);
+    return data.conCategory1 as Nested;
+  } catch (e) {
+    console.log('브랜드+상품리스트 API 에러');
+  }
+};
