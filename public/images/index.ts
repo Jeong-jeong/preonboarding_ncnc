a211402f1b1ef4a61aa6f2a4980e8df011b2f751
Jeongs <kimm.jeongs@gmail.com>
2022-02-14 17:39:54 +0900

    :heavy_plus_sign:  Dep: inline-react-svg 플러그인 설치, 이미지 경로 수정

---
+++ public/images/index.ts
@@ -0,0 +1,4 @@
+export { default as IcoArrow } from './arrow.svg';
+export { default as IcoClose } from './close.png';
+export { default as IcoMenuBar } from './menubar.png';
+export { default as IcoPencil } from './pencil.png';
