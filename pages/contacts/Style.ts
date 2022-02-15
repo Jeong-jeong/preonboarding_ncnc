7ce6fdf5455ecc487ffeb3b7a115d0f5eb44be79
Jeongs <kimm.jeongs@gmail.com>
2022-02-15 23:15:48 +0900

    :lipstick:  UI: Menubar 자리 생성

--- pages/contacts/Style.ts
+++ pages/contacts/Style.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
-import { hideScrollbar } from 'styles/commonStyle';
+import { hideScrollbar, flexbox } from 'styles/commonStyle';
 import { font14, font16 } from 'styles/font';
 
 export const ContactPageWrapper = styled.div`
   // @FIXME: scrollbar 숨기기
   ${hideScrollbar};
+  margin-top: ${({ theme }) => `${theme.size.baseHeight}px`};
 `;
 
 export const ContactInfos = styled.section`
