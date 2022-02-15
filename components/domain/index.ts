9c657e2f2dbbccc2a8df275e46bd7bcfcb84eba0
ludacirs <ludacirs@naver.com>
2022-02-15 19:13:26 +0900

    ✨ Feat: ProductionList 개선
    링크 매칭, 로직 수정

--- components/domain/index.ts
+++ components/domain/index.ts
@@ -1,3 +1,4 @@
 export { default as ProductionItem } from './ProductionItem/ProductionItem';
+export { default as ProductionList } from './ProductionList/ProductionList';
 export { default as Dropdown } from './Dropdown/Dropdown';
 export { default as DropdownList } from './DropdownList/DropdownList';
