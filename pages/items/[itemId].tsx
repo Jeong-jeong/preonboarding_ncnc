86434098db746479e0ccf0f7d04261733b194e60
sueyon-hong <bellahong95@gmail.com>
2022-02-15 18:55:31 +0900

    :sparkles:  Feat: Items 페이지 구현
    Closes #23

--- pages/items/[itemId].tsx
+++ pages/items/[itemId].tsx
@@ -2,21 +2,53 @@ import { GetServerSideProps } from 'next';
 import styled from 'styled-components';
 import React from 'react';
 import { ParsedUrlQuery } from 'querystring';
-import { ConItem } from 'types';
+import { IConItem } from 'types';
 import { getConItem } from 'api';
+import { MenuBar, OptionBox, BuyButton } from 'components/base';
+import { ProductionItem } from 'components/domain';
+import { IcoArrow } from 'public/images';
+import { useOption } from 'hooks';
 
 const ItemsPageWrapper = styled.div``;
 
 interface ItemsPageProps {
   data: {
-    conItem: ConItem;
+    conItem: IConItem;
   };
 }
 
 const ItemsPage = ({ data }: ItemsPageProps) => {
   const { conItem } = data;
+  const { toggle, isActive, isChecked, setIsChecked } = useOption({ initialState: false });
 
-  return <ItemsPageWrapper>ItemsPage</ItemsPageWrapper>;
+  return (
+    <ItemsPageWrapper>
+      <MenuBar onClick={() => console.log('clicked')}>
+        <IcoArrow />
+      </MenuBar>
+      <ProductionItem
+        imageUrl={conItem.imageUrl}
+        productionName={conItem.name}
+        minSellingPrice={conItem.minSellingPrice}
+        originalPrice={conItem.originalPrice}
+        size="big"
+        brandName={conItem.conCategory2.name}
+        productionId={conItem.id}
+      />
+      <OptionBox
+        data={conItem}
+        toggle={toggle}
+        isActive={isActive}
+        isChecked={isChecked}
+        setIsChecked={setIsChecked}
+      />
+      <BuyButton
+        children={isActive ? '구매하기' : isChecked ? '구매하기' : '옵션 선택하기'}
+        checkValue={!isActive}
+        toggle={toggle}
+      />
+    </ItemsPageWrapper>
+  );
 };
 
 interface Params extends ParsedUrlQuery {
