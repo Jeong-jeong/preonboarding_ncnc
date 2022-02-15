7dd12702fbe03e281e477831b6dea8d1755d58f5
sueyon-hong <bellahong95@gmail.com>
2022-02-15 19:38:42 +0900

    :bug:  Fix: Menubar 로직수정, 스타일 추가

--- components/base/MenuBar/Style.ts
+++ components/base/MenuBar/Style.ts
@@ -5,11 +5,25 @@ import { font15 } from 'styles/font';
 export const HeaderInner = styled.div`
   ${flexbox({ jc: 'between' })};
   max-width: 1440px;
-  height: 100%;
-  margin: 0 auto;
+  height: 50px;
   padding: 7px 19px;
 `;
 
 export const Title = styled.h1`
   ${font15(600)}
+`;
+
+export const SvgWrapper = styled.div`
+  transform: rotate(-90deg);
+  cursor: pointer;
+
+  > svg {
+    width: 20px;
+    height: 20px;
+
+    > path {
+      fill: ${({ theme }) => theme.colors.primary};
+      transform: scale(1.1);
+    }
+  }
 `;
