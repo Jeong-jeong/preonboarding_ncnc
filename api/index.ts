b231b8408f6e92bd2bd00e848562af77c793dc6b
ludacirs <ludacirs@naver.com>
2022-02-15 13:56:50 +0900

    ✨ Feat: Contact Page 데이터 패칭 구현

--- api/index.ts
+++ api/index.ts
@@ -27,3 +27,21 @@ export const getNested = async (categoryId: number) => {
     console.log('브랜드+상품리스트 API 에러');
   }
 };
+
+export const getFAQTypes = async () => {
+  try {
+    const { data } = await axios.get(`https://api2.ncnc.app/qa-types`);
+    return data.qaTypes as QaTypes[];
+  } catch (e) {
+    console.log('FAQ Types API 에러');
+  }
+};
+
+export const getQas = async (qaTypeId: number) => {
+  try {
+    const { data } = await axios.get(`https://api2.ncnc.app/qas?qaTypeId=${qaTypeId}`);
+    return data.qas as Qas[];
+  } catch (e) {
+    console.log('Qas API 에러');
+  }
+};
