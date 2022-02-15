0fe7c8b209309f7f56eb0e0dd6d59ecca9d774ba
kokoball <kbp4154@naver.com>
2022-02-15 19:42:07 +0900

    :bug:  Fix: Production~ 수정
    Closes #20

--- components/domain/index.ts
+++ components/domain/index.ts
@@ -1,4 +1,5 @@
 export { default as ProductionItem } from './ProductionItem/ProductionItem';
+export { default as ProductionList } from './ProductionList/ProductionList';
 export { default as Dropdown } from './Dropdown/Dropdown';
 export { default as DropdownList } from './DropdownList/DropdownList';
 export { default as Swiper } from './Swiper/Swiper';
