b6b39678c599757ced74c67b99fa666317ab356e
ludacirs <ludacirs@naver.com>
2022-02-16 02:06:09 +0900

    🚀 Deploy: vercel 빌드 에러 수정

--- components/base/Header/Header.tsx
+++ components/base/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
-import Link from 'next/Link';
+import Link from 'next/link';
 import * as S from './Style';
 import { MenuBar } from 'components/base';
 import { useWindowWidth } from 'hooks';
