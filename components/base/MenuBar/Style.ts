c235d75f86b206d43a528fdeeebe9b9e7d119653
Jeongs <kimm.jeongs@gmail.com>
2022-02-15 22:46:25 +0900

    :bug:  Fix: Menubar height 상수화, 백그라운드 추가

--- components/base/MenuBar/Style.ts
+++ components/base/MenuBar/Style.ts
@@ -5,11 +5,12 @@ import { font15 } from 'styles/font';
 export const HeaderInner = styled.div`
   ${flexbox({ jc: 'between' })};
   max-width: 1440px;
-  height: 100%;
-  margin: 0 auto;
+  height: ${({ theme }) => `${theme.size.baseHeight}px`};
   padding: 7px 19px;
+  margin: 0 auto;
+  background-color: ${({ theme }) => theme.colors.white};
 `;
 
 export const Title = styled.h1`
-  ${font15(600)}
+  ${font15(600)};
 `;
