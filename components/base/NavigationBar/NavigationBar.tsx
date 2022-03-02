86e9520a79bccc547d3c18f48cfbc72508cbb92d
Jeongs <kimm.jeongs@gmail.com>
2022-03-02 16:03:41 +0900

    :recycle:  Refactor: 렌더링 시 active된 DOM 요소로 스크롤 in categories

--- components/base/NavigationBar/NavigationBar.tsx
+++ components/base/NavigationBar/NavigationBar.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { ICategory } from 'types';
 import { NavigationItem } from 'components/base';
-import { useLocalStorage } from 'hooks';
 import * as S from './Style';
 
 interface NavigationBarProps {
@@ -10,33 +9,22 @@ interface NavigationBarProps {
 }
 const NavigationBar = ({ categories, categoryId }: NavigationBarProps) => {
   const scrolledRef = useRef<HTMLUListElement>(null);
-  const [storedValue, setValue] = useLocalStorage('navigationAcitvedX', 0);
-
-  const handleClick = (e: React.MouseEvent<HTMLLIElement> | React.TouchEvent<HTMLLIElement>) => {
-    // @NOTE: 새로고침 후에도 actived 위치 유지
-    const ul = e.currentTarget?.closest('ul');
-    if (ul !== null) {
-      setValue(ul.scrollLeft);
-    }
-  };
 
   useEffect(() => {
-    if (scrolledRef.current) {
-      scrolledRef.current.scrollTo(storedValue, 0);
+    // @NOTE: 렌더링 시 active된 DOM 요소로 navigation scroll
+    const target = document.querySelector('[data-active="true"]');
+
+    if (target) {
+      target.scrollIntoView();
     }
-  }, [storedValue]);
+  }, [categories]);
 
   return (
     <S.NavigationBarWrapper>
       <S.SlideWrapper ref={scrolledRef}>
         {React.Children.toArray(
           categories?.map(({ name, id }) => (
-            <NavigationItem
-              name={name}
-              id={id}
-              active={id === +categoryId ?? false}
-              onClick={handleClick}
-            />
+            <NavigationItem name={name} id={id} active={id === +categoryId ?? false} />
           )),
         )}
       </S.SlideWrapper>
--- components/base/NavigationItem/NavigationItem.tsx
+++ components/base/NavigationItem/NavigationItem.tsx
@@ -1,17 +1,15 @@
 import Link from 'next/link';
-import { MouseEventHandler } from 'react';
 import * as S from './Style';
 
 interface NavigationItemProps {
   name: string;
   id: number;
   active: boolean;
-  onClick: MouseEventHandler<HTMLLIElement>;
 }
 
-const NavigationItem = ({ name, id, active, onClick }: NavigationItemProps) => {
+const NavigationItem = ({ name, id, active }: NavigationItemProps) => {
   return (
-    <li onClick={onClick}>
+    <li data-active={active}>
       <Link href={`/categories/${id}`} passHref>
         <S.NavigationItemWrapper active={active}>{name}</S.NavigationItemWrapper>
       </Link>
