b66abbb3e8345b5c73f7f79cbd4d548d9babadfd
Jeongs <kimm.jeongs@gmail.com>
2022-03-07 16:33:25 +0900

    :wrench:  Config: images domain   가 in next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ module.exports = {
   reactStrictMode: true,
   images: {
     // 외부 url일 경우 보안적인 이유로 domain 적어주기
-    domains: ['d2lfdrnysfmya6.cloudfront.net', 'd1dsr05o5i286u.cloudfront.net', 'unsplash.it'],
+    domains: [
+      'd2lfdrnysfmya6.cloudfront.net',
+      'd1dsr05o5i286u.cloudfront.net',
+      'd1tftw9jjs9bx5.cloudfront.net',
+      'unsplash.it',
+    ],
   },
 };
